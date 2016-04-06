807ddb26390e36b84ded89714486cadfb70dc717
ericmcg <ericmcg@gmail.com>
2016-04-06 19:23:29 +0100

    Initial Commit

---
+++ Stealth Outdoor Lighting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stealth Outdoor Lighting
+//
+//  Created by Eric McGaughey on 4/6/16.
+//  Copyright © 2016 Eric McGaughey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
